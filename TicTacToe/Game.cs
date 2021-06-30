3a2dcb6d7f889efd9716df8f05e56aa28c5d688b
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-06-30 18:51:13 +0200

    Update Game.cs

--- TicTacToe/Game.cs
+++ TicTacToe/Game.cs
@@ -1,67 +1,46 @@
-﻿using System;
+using System;
 
 namespace TicTacToe
 {
     class Game
     {
-        private readonly Cell[] _cells;
         private readonly Cell[][] _rows;
         private readonly Cell[][] _columns;
         private readonly Player Player1;
         private readonly Player Player2;
-        public Player Winner;
+        private Player winner;
 
-        public Game(Cell[] cells, Cell[][] rows, Cell[][] columns,
+        public Game(Cell[][] rows, Cell[][] columns,
             Player player1, Player player2)
         {
-            _cells = cells;
-            _columns = columns;
             _rows = rows;
+            _columns = columns;
             Player1 = player1;
             Player2 = player2;
+            RunGame();
         }
 
-        public void Run()
+        private void RunGame()
         {
             while (true)
             {
-                TakeCoordinate(Player1);
+                Player1.GetCoordinate();
                 IsWinCondition(_rows);
                 IsWinCondition(_columns);
-                if (Winner != null) break;
-                TakeCoordinate(Player2);
+                if (winner != null) break;
+                Player2.GetCoordinate();
                 IsWinCondition(_rows);
                 IsWinCondition(_columns);
-                if (Winner != null) break;
+                if (winner != null) break;
             }
             PrintWinner();
         }
 
-        public void TakeCoordinate(Player player)
+        private void IsWinCondition(Cell[][] cellStructures)
         {
-            Console.WriteLine($"{player.PlayerUserName}: Enter a coordinate");
-            string coordinate = Console.ReadLine();
-            SetCellState(coordinate, player);
-        }
-
-        private void SetCellState(string coordinate, Player player)
-        {
-            for(int i = 0; i < _cells.Length; i++)
+            for (int i = 0; i < cellStructures.Length; i++)
             {
-                Cell cell = _cells[i];
-                if (cell.Coordinate == coordinate)
-                {
-                    cell.SetPlayer(player);
-                    break;
-                }
-            }
-        }
-
-        public void IsWinCondition(Cell[][] cellStructure)
-        {
-            for (int i = 0; i < cellStructure.Length; i++)
-            {
-                Cell[] cells = cellStructure[i];
+                Cell[] cells = cellStructures[i];
                 int player1Count = 0;
                 int player2Count = 0;
                 for (int j = 0; j < cells.Length; j++)
@@ -70,14 +49,14 @@ namespace TicTacToe
                     if (cell.IsMarkedByPlayer1) player1Count++;
                     if (cell.IsMarkedByPlayer2) player2Count++;
                 }
-                if (player1Count == 3) Winner = Player1;
-                if (player2Count == 3) Winner = Player2;
+                if (player1Count == 3) winner = Player1;
+                if (player2Count == 3) winner = Player2;
             }
         }
 
         private void PrintWinner()
         {
-            Console.WriteLine($"{Winner.PlayerUserName} wins.");
+            Console.WriteLine($"{winner.UserName} wins.");
         }
     }
 }
