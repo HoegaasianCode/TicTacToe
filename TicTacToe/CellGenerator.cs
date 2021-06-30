d0ba51355b7d4c43a97c0a5fc941906a27218a51
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-06-30 18:50:00 +0200

    Update CellGenerator.cs

--- TicTacToe/CellGenerator.cs
+++ TicTacToe/CellGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +8,21 @@ namespace TicTacToe
 {
     class CellGenerator
     {
-        public Cell[] Cells { get; set; }
-        public Cell[][] Rows { get; set; }
-        public Cell[][] Columns { get; set; }
+        private Cell[] cells;
+        private Cell[][] rows;
+        private Cell[][] columns;
 
-        public void CreateCells()
+        public CellGenerator()
         {
-            Cell cell0 = new("A1");
+            CreateCells();
+            CreateRows();
+            CreateColumns();
+            CreatePlayers();
+        }
+
+        private void CreateCells()
+        {
+            Cell cell = new("A1");
             Cell cell1 = new("A2");
             Cell cell2 = new("A3");
             Cell cell3 = new("B1");
@@ -23,24 +31,26 @@ namespace TicTacToe
             Cell cell6 = new("C1");
             Cell cell7 = new("C2");
             Cell cell8 = new("C3");
-            Cells = new Cell[] {cell0, cell1, cell2, cell3, cell4,
+            cells = new Cell[] {cell, cell1, cell2, cell3, cell4,
             cell5, cell6, cell7, cell8};
         }
 
-        public void CreateCellRows()
+        private void CreateRows()
         {
-            Cell[] row0 = new Cell[] { Cells[0], Cells[1], Cells[2] };
-            Cell[] row1 = new Cell[] { Cells[3], Cells[4], Cells[5] };
-            Cell[] row2 = new Cell[] { Cells[6], Cells[7], Cells[8] };
-            Rows = new Cell[][] {row0, row1, row2};
+            Cell[] row = new Cell[] { cells[0], cells[1], cells[2] };
+            Cell[] row1 = new Cell[] { cells[3], cells[4], cells[5] };
+            Cell[] row2 = new Cell[] { cells[6], cells[7], cells[8] };
+            rows = new Cell[][] {row, row1, row2};
         }
 
-        public void CreateCellColumns()
+        private void CreateColumns()
         {
-            Cell[] column1 = new Cell[] { Cells[0], Cells[3], Cells[6] };
-            Cell[] column2 = new Cell[] { Cells[1], Cells[4], Cells[7] };
-            Cell[] column3 = new Cell[] { Cells[2], Cells[5], Cells[8] };
-            Columns = new Cell[][] {column1, column2, column3};
+            Cell[] column = new Cell[] { cells[0], cells[3], cells[6] };
+            Cell[] column1 = new Cell[] { cells[1], cells[4], cells[7] };
+            Cell[] column2 = new Cell[] { cells[2], cells[5], cells[8] };
+            columns = new Cell[][] {column, column1, column2};
         }
+
+        private void CreatePlayers() => new PlayerGenerator(cells, rows, columns);
     }
 }
